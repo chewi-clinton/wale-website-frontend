464d8892b4e66364b575257b953d5051d897e551
chewi_clinton <chewiclinton.shu@ictuniversity.edu.cm>
2025-09-17 15:50:44 +0100

    Added map to about section

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -146,6 +146,22 @@ const AboutPage = () => {
           </p>
         </div>
       </section>
+
+      {/* Google Map Section */}
+      <section className="map-section">
+        <h2>Find Us Here</h2>
+        <div className="map-container">
+          <iframe
+            title="Trimaxa Pharmacy Location"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3021.3034192797637!2d-74.17419952352454!3d40.754063935186955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25378bdf42eb9%3A0x42c606a785467a4!2s1299%20McCarter%20Hwy%2C%20Newark%2C%20NJ%2007104%2C%20USA!5e0!3m2!1sen!2sus!4v1695673643659!5m2!1sen!2sus"
+            width="100%"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+          ></iframe>
+        </div>
+      </section>
     </div>
   );
 };
