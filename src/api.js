fe3568949040b0b441e2cab105e76b02c3154469
chewi_clinton <chewiclinton.shu@ictuniversity.edu.cm>
2025-09-30 01:11:07 +0100

    fix: update baseURL in axios instance to include protocol

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "backend.trimaxapharmacy.com",
+  baseURL: "https://backend.trimaxapharmacy.com",
   headers: {
     "Content-Type": "application/json",
   },
